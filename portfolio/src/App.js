93145aaec6b219a9737521cc1b73861e587f8c3c
ACoolCat <36032266+ACoolCat@users.noreply.github.com>
2021-02-17 13:59:05 -0500

    bio is connected

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Bio from './bio/bio.js';
 
+
+
+
+
+function App() {
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1> Welcome to my site </h1>
-
+        <Bio/>
       </header>
     </div>
   );
---
+++ portfolio/src/bio/bio.js
@@ -0,0 +1,10 @@
+function Bio() {
+
+  return (
+    <div className="Bio">
+        <h1> TIS WEDNESDAY SAILERS. </h1>
+    </div>
+  );
+}
+
+export default Bio;
