32693d1002ab663cf31d55558b82f977c1ca0f6f
ACoolCat <36032266+ACoolCat@users.noreply.github.com>
2021-02-17 14:57:59 -0500

    completed bio

--- portfolio/src/bio/bio.js
+++ portfolio/src/bio/bio.js
@@ -1,8 +1,13 @@
+import './bio.css';
+
 function Bio() {
 
   return (
     <div className="Bio">
-        <h1> TIS WEDNESDAY SAILERS. </h1>
+        <h2> Welcome</h2>
+        <div className="container">
+        <p>My name is Christian Lima Sullivan, and welcome to my page. I am a software engineer who knows a multitude of different langauges. Having been coding since I was back in Middleschool, I have a passion for the craft and wish to bring that pasion to a future employer. </p>
+        </div>
     </div>
   );
 }
